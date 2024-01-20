60ed2fa70908b25fa60049a7495243f7936be0fc
Devansh Rautela <maildevanshrautela@gmail.com>
2024-01-21 02:43:08 +0530

    commit

---
+++ Client/test.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class test {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+        String input = sc.nextLine();
+        System.out.println("Hi "+input+", this is a demo file for java FTP Application");
+    }
+}
